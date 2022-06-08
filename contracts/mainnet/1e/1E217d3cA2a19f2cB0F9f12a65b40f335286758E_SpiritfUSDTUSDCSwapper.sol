5fceb8c6ab54be16feba55596f57e603bca010f9
minion <minion>
2022-06-08 21:07:00 +0000

    with 💕

---
+++ contracts/mainnet/1e/1E217d3cA2a19f2cB0F9f12a65b40f335286758E_SpiritfUSDTUSDCSwapper.sol
@@ -0,0 +1,365 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0;
+
+interface IUniswapV2Pair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint);
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function kLast() external view returns (uint);
+
+    function mint(address to) external returns (uint liquidity);
+    function burn(address to) external returns (uint amount0, uint amount1);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function skim(address to) external;
+    function sync() external;
+
+    function initialize(address, address) external;
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+import "./IERC20.sol";
+
+interface IBentoBoxV1 {
+    function toAmount(
+        address _token,
+        uint256 _share,
+        bool _roundUp
+    ) external view returns (uint256);
+
+    function withdraw(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 share
+    ) external returns (uint256, uint256);
+
+    function deposit(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 share
+    ) external returns (uint256, uint256);
+
+    function deploy(
+        address masterContract,
+        bytes calldata data,
+        bool useCreate2
+    ) external payable returns (address cloneAddress);
+
+    function setMasterContractApproval(
+        address user,
+        address masterContract,
+        bool approved,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    function balanceOf(IERC20, address) external view returns (uint256);
+
+    function totals(IERC20) external view returns (uint128 elastic, uint128 base);
+
+    function flashLoan(
+        address borrower,
+        address receiver,
+        IERC20 token,
+        uint256 amount,
+        bytes calldata data
+    ) external;
+
+    function toShare(
+        address token,
+        uint256 amount,
+        bool roundUp
+    ) external view returns (uint256 share);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+interface IERC20 {
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /// @notice EIP 2612
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+interface IPopsicle {
+    function pool() external view returns (address);
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function strategy() external view returns (address);
+
+    function usersAmounts() external view returns (uint256 amount0, uint256 amount1);
+
+    function totalSupply() external view returns (uint256 amount);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function withdraw(uint256 shares, address to) external returns (uint256 amount0, uint256 amount1);
+
+    function tickLower() external view returns (int24);
+
+    function tickUpper() external view returns (int24);
+
+    function deposit(
+        uint256 amount0Desired,
+        uint256 amount1Desired,
+        address to
+    )
+        external
+        returns (
+            uint256 shares,
+            uint256 amount0,
+            uint256 amount1
+        );
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.6.12;
+
+import "./IERC20.sol";
+
+interface ISwapperGeneric {
+    /// @notice Withdraws 'amountFrom' of token 'from' from the BentoBox account for this swapper.
+    /// Swaps it for at least 'amountToMin' of token 'to'.
+    /// Transfers the swapped tokens of 'to' into the BentoBox using a plain ERC20 transfer.
+    /// Returns the amount of tokens 'to' transferred to BentoBox.
+    /// (The BentoBox skim function will be used by the caller to get the swapped funds).
+    function swap(
+        IERC20 fromToken,
+        IERC20 toToken,
+        address recipient,
+        uint256 shareToMin,
+        uint256 shareFrom
+    ) external returns (uint256 extraShare, uint256 shareReturned);
+
+    /// @notice Calculates the amount of token 'from' needed to complete the swap (amountFrom),
+    /// this should be less than or equal to amountFromMax.
+    /// Withdraws 'amountFrom' of token 'from' from the BentoBox account for this swapper.
+    /// Swaps it for exactly 'exactAmountTo' of token 'to'.
+    /// Transfers the swapped tokens of 'to' into the BentoBox using a plain ERC20 transfer.
+    /// Transfers allocated, but unused 'from' tokens within the BentoBox to 'refundTo' (amountFromMax - amountFrom).
+    /// Returns the amount of 'from' tokens withdrawn from BentoBox (amountFrom).
+    /// (The BentoBox skim function will be used by the caller to get the swapped funds).
+    function swapExact(
+        IERC20 fromToken,
+        IERC20 toToken,
+        address recipient,
+        address refundTo,
+        uint256 shareFromSupplied,
+        uint256 shareToExact
+    ) external returns (uint256 shareUsed, uint256 shareReturned);
+}
+
+// SPDX-License-Identifier: MIT
+// solhint-disable func-name-mixedcase, var-name-mixedcase
+pragma solidity >=0.6.12;
+
+interface CurvePool {
+    function coins(uint256 i) external view returns (address);
+
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external returns (uint256);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external returns (uint256);
+
+    function exchange(
+        uint256 i,
+        uint256 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external returns (uint256);
+
+    function get_dy_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function get_dy(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function approve(address _spender, uint256 _value) external returns (bool);
+
+    function add_liquidity(uint256[2] memory amounts, uint256 _min_mint_amount) external;
+
+    function add_liquidity(uint256[3] memory amounts, uint256 _min_mint_amount) external;
+
+    function add_liquidity(uint256[4] memory amounts, uint256 _min_mint_amount) external;
+
+    function remove_liquidity_one_coin(
+        uint256 tokenAmount,
+        int128 i,
+        uint256 min_amount
+    ) external returns (uint256);
+
+    function remove_liquidity_one_coin(
+        uint256 tokenAmount,
+        uint256 i,
+        uint256 min_amount
+    ) external returns (uint256);
+
+    function remove_liquidity_one_coin(
+        uint256 tokenAmount,
+        int128 i,
+        uint256 min_amount,
+        address receiver
+    ) external returns (uint256);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Pair.sol";
+import "../../interfaces/ISwapperGeneric.sol";
+import "../../interfaces/IPopsicle.sol";
+import "../../interfaces/IBentoBoxV1.sol";
+import "../../interfaces/curve/ICurvePool.sol";
+
+contract SpiritfUSDTUSDCSwapper is ISwapperGeneric {
+    IBentoBoxV1 public immutable bentoBox;
+
+    IUniswapV2Pair public constant LP = IUniswapV2Pair(0xe7F86CEf8FEf60ce5050899D1F8e465C00D04a79);
+
+    CurvePool public constant MIM3POOL = CurvePool(0x2dd7C9371965472E5A5fD28fbE165007c61439E1);
+    IERC20 public constant MIM = IERC20(0x82f0B8B456c1A451378467398982d4834b6829c1);
+    IERC20 public constant USDC = IERC20(0x04068DA6C83AFCFA0e13ba15A6696662335D5B75);
+    IERC20 public constant FUSDT = IERC20(0x049d68029688eAbF473097a2fC38ef61633A3C7A);
+
+    constructor(IBentoBoxV1 _bentoBox) {
+        bentoBox = _bentoBox;
+
+        USDC.approve(address(MIM3POOL), type(uint256).max);
+        FUSDT.approve(address(MIM3POOL), type(uint256).max);
+    }
+
+    /// @inheritdoc ISwapperGeneric
+    function swap(
+        IERC20,
+        IERC20,
+        address recipient,
+        uint256 shareToMin,
+        uint256 shareFrom
+    ) public override returns (uint256 extraShare, uint256 shareReturned) {
+        (uint256 amountFrom, ) = bentoBox.withdraw(IERC20(address(LP)), address(this), address(this), 0, shareFrom);
+
+        // LP -> USDC/fUSDT
+        LP.transfer(address(LP), amountFrom);
+        (uint256 usdcAmount, uint256 fusdtAmount) = LP.burn(address(this));
+
+        // fUSDT -> MIM
+        uint256 mimAmount = MIM3POOL.exchange(1, 0, fusdtAmount, 0, address(bentoBox));
+
+        // USDC -> MIM
+        mimAmount += MIM3POOL.exchange(2, 0, usdcAmount, 0, address(bentoBox));
+
+        (, shareReturned) = bentoBox.deposit(MIM, address(bentoBox), recipient, mimAmount, 0);
+        extraShare = shareReturned - shareToMin;
+    }
+
+    // Swaps to an exact amount, from a flexible input amount
+    /// @inheritdoc ISwapperGeneric
+    function swapExact(
+        IERC20,
+        IERC20,
+        address,
+        address,
+        uint256,
+        uint256
+    ) public pure virtual returns (uint256 shareUsed, uint256 shareReturned) {
+        return (0, 0);
+    }
+}
