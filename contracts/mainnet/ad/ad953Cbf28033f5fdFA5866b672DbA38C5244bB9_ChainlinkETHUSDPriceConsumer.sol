bd24a0a1a4383f04f7fd14d3baf28ffcdd8e10cf
minion <minion>
2022-07-08 21:19:21 +0000

    with 💕

---
+++ contracts/mainnet/ad/ad953Cbf28033f5fdFA5866b672DbA38C5244bB9_ChainlinkETHUSDPriceConsumer.sol
@@ -0,0 +1,62 @@
+/**
+ *Submitted for verification at FtmScan.com on 2022-07-08
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+interface AggregatorV3Interface {
+
+  function decimals() external view returns (uint8);
+  function description() external view returns (string memory);
+  function version() external view returns (uint256);
+
+  // getRoundData and latestRoundData should both raise "No data present"
+  // if they do not have data to report, instead of returning unset values
+  // which could be misinterpreted as actual reported values.
+  function getRoundData(uint80 _roundId)
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+}
+pragma solidity >=0.6.11;
+contract ChainlinkETHUSDPriceConsumer {
+
+    AggregatorV3Interface internal priceFeed;
+
+
+    constructor () public {
+        priceFeed = AggregatorV3Interface(0xf4766552D15AE4d256Ad41B6cf2933482B0680dc);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (uint80 roundID, int price, , uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
+        require(price >= 0 && updatedAt!= 0 && answeredInRound >= roundID, "Invalid chainlink price");
+        
+        return price;
+    }
+
+    function getDecimals() public view returns (uint8) {
+        return priceFeed.decimals();
+    }
+}
