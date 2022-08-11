3913435898c4693196c8ac6a7c29af330ef49318
minion <minion>
2022-08-11 05:07:58 +0000

    with 💕

---
+++ contracts/mainnet/0d/0d765185982d58eb38a97637ce94aec71d394e2f_Recover.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at FtmScan.com on 2022-08-10
+*/
+
+pragma solidity ^0.4.19;
+
+contract Recover {
+  function ecr (bytes32 msgh, uint8 v, bytes32 r, bytes32 s) public pure
+  returns (address sender) {
+    return ecrecover(msgh, v, r, s);
+  }
+}
---
+++ contracts/mainnet/8c/8C7407C97425e889A36955b56d038C85A01b4719_ThreePoolOracle.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+interface IOracle {
+    /// @notice Get the latest exchange rate.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return success if no valid (recent) rate is available, return false else true.
+    /// @return rate The rate of the requested asset / pair / pool.
+    function get(bytes calldata data) external returns (bool success, uint256 rate);
+
+    /// @notice Check the last exchange rate without any state changes.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return success if no valid (recent) rate is available, return false else true.
+    /// @return rate The rate of the requested asset / pair / pool.
+    function peek(bytes calldata data) external view returns (bool success, uint256 rate);
+
+    /// @notice Check the current spot exchange rate without any state changes. For oracles like TWAP this will be different from peek().
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return rate The rate of the requested asset / pair / pool.
+    function peekSpot(bytes calldata data) external view returns (uint256 rate);
+
+    /// @notice Returns a human readable (short) name about this oracle.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return (string) A human readable symbol name about this oracle.
+    function symbol(bytes calldata data) external view returns (string memory);
+
+    /// @notice Returns a human readable name about this oracle.
+    /// @param data Usually abi encoded, implementation specific data that contains information and arguments to & about the oracle.
+    /// For example:
+    /// (string memory collateralSymbol, string memory assetSymbol, uint256 division) = abi.decode(data, (string, string, uint256));
+    /// @return (string) A human readable name about this oracle.
+    function name(bytes calldata data) external view returns (string memory);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+import "../interfaces/IOracle.sol";
+
+// Chainlink Aggregator
+
+interface IAggregator {
+    function latestAnswer() external view returns (int256 answer);
+}
+
+interface IYearnVault {
+    function pricePerShare() external view returns (uint256 price);
+}
+
+interface ICurvePool {
+    function get_virtual_price() external view returns (uint256 price);
+}
+
+contract ThreePoolOracle is IOracle {
+    ICurvePool public constant threecrv = ICurvePool(0x2dd7C9371965472E5A5fD28fbE165007c61439E1);
+    IAggregator public constant MIM = IAggregator(0x28de48D3291F31F839274B8d82691c77DF1c5ceD);
+    IAggregator public constant USDC = IAggregator(0x2553f4eeb82d5A26427b8d1106C51499CBa5D99c);
+    IAggregator public constant USDT = IAggregator(0xF64b636c5dFe1d3555A847341cDC449f612307d0);
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    // FROM: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/6d97f0919547df11be9443b54af2d90631eaa733/contracts/utils/math/Math.sol
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    // Calculates the lastest exchange rate
+    // Uses both divide and multiply only for tokens not supported directly by Chainlink, for example MKR/USD
+    function _get() internal view returns (uint256) {
+        // As the price should never be negative, the unchecked conversion is acceptable
+        uint256 minStable = min(uint256(MIM.latestAnswer()), min(uint256(USDC.latestAnswer()), uint256(USDT.latestAnswer())));
+        //get_virtual_price is available for plain pools (stableSwap between pegged assets)
+        uint256 yVCurvePrice = threecrv.get_virtual_price() * minStable;
+
+        return 1e44 / yVCurvePrice;
+    }
+
+    // Get the latest exchange rate
+    /// @inheritdoc IOracle
+    function get(bytes calldata) public view override returns (bool, uint256) {
+        return (true, _get());
+    }
+
+    // Check the last exchange rate without any state changes
+    /// @inheritdoc IOracle
+    function peek(bytes calldata) public view override returns (bool, uint256) {
+        return (true, _get());
+    }
+
+    // Check the current spot exchange rate without any state changes
+    /// @inheritdoc IOracle
+    function peekSpot(bytes calldata data) external view override returns (uint256 rate) {
+        (, rate) = peek(data);
+    }
+
+    /// @inheritdoc IOracle
+    function name(bytes calldata) public pure override returns (string memory) {
+        return "Chainlink 3Pool";
+    }
+
+    /// @inheritdoc IOracle
+    function symbol(bytes calldata) public pure override returns (string memory) {
+        return "LINK/3Pool";
+    }
+}
