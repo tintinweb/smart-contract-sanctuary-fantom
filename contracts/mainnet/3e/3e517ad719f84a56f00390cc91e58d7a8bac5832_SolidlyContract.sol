1383d5bd2596f00611f59701a2f1463ede8fa22d
minion <minion>
2022-08-18 09:25:13 +0000

    with 💕

---
+++ contracts/mainnet/3e/3e517ad719f84a56f00390cc91e58d7a8bac5832_SolidlyContract.sol
@@ -0,0 +1,126 @@
+/**
+ *Submitted for verification at FtmScan.com on 2022-08-17
+*/
+
+//The encrypted PGP Message below contains an agreement between the "Client" and the "Developer" as defined per Agreement
+//Each of the 2 representatives of both parties can decrypt the PGP message below with their private key associated with their public key
+//The 4 public keys able to decrypt the message below are as follows:
+/**Key 1:-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQGNBGL9UtIBDADs3jLht2H5jpUFBHqtjy++8Ink4va6p4aJ7t3DbFW8uO9wTly0
+h6jOzv3D8rS8V8f+Sio5rrqb3el8tVo6Ewt6KkXfMnHsgjmooJwLgYxvW6z5pdeA
+vThZXgUf4w8qYCdag9mcE5EQcEGz/s/BiNb3R1lAjcJn6KiQyQ0rcIYsyrqfWlgs
+EFMKTnDELI+Q3MDpYou7iLc1UqtlfqkYO6v1n2L2fxNuvdxw3mGNXxyJUxRKnfJZ
+haq2dgyqUhfkWgST2PH0vlYdJjE1uD/uwNb+XiEZuLlb72afNi3ydKTcSiIhPMIM
+AYwHDZNjX67RwSomQY655jQwPMGvdR/FVrd1ej3S2FQJuS4vyDMrJJKDIvImjG+N
+lvSrNc96/SxWUYk4k/dVU8T4XsaMfec26kcjany4Ke5kUnIU07qY82Py5j26GhXb
+V07e8UCPJhyqAJlP/Bsk4rykV2w3f+v9q8YIYTcjiJC0INkGNP6hJuCF82yJVbct
+TBOHH/AOoWkOISUAEQEAAbQKYzMwX2NyeXB0b4kB1AQTAQgAPhYhBOu5smsQ+JUg
+oe4rFByPRa0D8Wh/BQJi/VLSAhsDBQkDwmcABQsJCAcCBhUKCQgLAgQWAgMBAh4B
+AheAAAoJEByPRa0D8Wh/91QMALs5xMCmgTXVXqE7Xx7hznVpe8s4yyF/08ilUW8D
+Y0Bpd/7OLJOCARzvAvs4RVH4/nnPaOku2sCJIEgo+j4HGvoY8KiZ4ZHiOqzC4MEE
+uIwAIWWvSrVxV3mhwI9HGKKlV9WsdX2h7kCcPNBnywNKIEAzTQbgSar8X03+Y9Ab
+FQlFFeYYGT/M5C8gKB7iafh4q4eAmUJP8nod4iXAmc02p8dq1o43hF7i6Svcch19
+3rH7QWaQLbluR49sYYMLm0Ts8y+YwXjCTvQ5+qUlDVDK69ah+7IA20QRLVl0cwDB
+UJwlil6ChzQ4JzgmCr51wJ1IpENQwKxY4xdMORjBlB32N1xESkegVu3J4GIU3b/M
+zEkx3P8c+AjYIyZ6I1dNvGik50d/uxBopKNEY5L+OB0R4hAbbJTU1pwKJq64XW2L
+9bmNAO1KOSocd8gIdUN9h79N5vqg3UBUG1FMPgUSJgJ49eawUnndtgpO0Ffk7yao
+WhnHLT0z+AenzyUiaRANq0oP9rkBjQRi/VLSAQwAyor7SbHZpIdGyghjeKveA7NB
+/C6a0SOjCgnHrF7LDBUTINF3CyQ2gisqdY1LF3r844ZxOb03RjehpX+qOGjAY022
+wrygZdGp9iWB3hbRkhGM5ZQy0zPEW6fwmw/AcJSoiJWBSUuabUbkRRBfGKcBWXOD
+gOtEPn0q77jqqevo6UPctV75vyQn+OGjyrfxuarjJP1hzWpIOstqWNOB/DCWtPxg
+9cKtS6MySYlaV0CYOUOJxwgVXceoDpJGoPBn2tFE3rHKSJZcEia2d+LPubhOlIwT
+7GiiLPM6A/ZsPmFz35UkZXe2cBzpTtkD08WlhqmjLASugWRdrmg3uPzT8OfMi2E7
+qD9VEDrKpt/mbVkAgkoS8DjCucxGGqNiACN5h6Pzo9WbS+nShb7JZXPrNfzoImmA
+3/3FFfUjvkno66LWJpOoctW1UGVBbZHAKfxWUZ2D6YvDSmcGvH6bxs1bTUfCUzcn
+jaiNM/fu36TTeO9jNsoEpP4cEg699yH4anUxne57ABEBAAGJAbwEGAEIACYWIQTr
+ubJrEPiVIKHuKxQcj0WtA/FofwUCYv1S0gIbDAUJA8JnAAAKCRAcj0WtA/Fof32w
+DADdG8qPR0FKLwIAsemY7pzwplTcv7kl7MQZNYaoT5lnpWLY0fjrxb6kZNU10IbN
+ostfEtNH/LlUrUNA4RKQhSidw54UJ1eyXv926C9sW6+E/xixnuSQBR90y2snOPNS
++bYl8x3NoBWVHYYBf5EOcyYUQctmTRFxPl1pP7nzQ8LOguu7rY9jgqX1nwTxsraj
+kwFraYigotUO6L1mkJ9lBOuDyuYWkqkoSRJF/Q3bvifBXpmA5yCgh3UbgdAHtkkL
+NhOMHdJ8ezRPfPDmOU4oTuquoKGqjzlc+Hdu/jJC719HvZwLoPHbfVnUgQbgQmBR
+Re3d6sb8jJv1BBZfjzJuhnNMKTfxy+ZPnsW8Ck4taospr4pvWi92LG39FtXT58Tt
+EKeqgpIoY58Vc3qpizan5wNfBh/AjyPNsfxPb2qiSTQgS3gLg2PCclJs7kYK0S7l
+OBlpJLOmgMpAlUUCs66nBX3XOhqySfDr1mmRhHHulXP68CVhx5Y6KtJHwrWyWZgX
+qig=
+=NANF
+-----END PGP PUBLIC KEY BLOCK-----
+*/
+//Key 2:
+//Key 3:
+//Key 4:
+
+//By signature of all 4 parties as defined per the Agreement, the encrypted Agreement below takes effect
+//Signatures are not reversible
+
+/**-----BEGIN PGP MESSAGE-----
+
+hQGMA+POrur4dvx+AQv7Be2l42WWvr/9lTJk3BYNPlRSk3fQPdgFlNlFBAcQbKSQ
+Gn8h3R3Bs0xNoiwLvUMp8Hm37BaMXjvvpPwLrjE8Gg/SRdd9nLx0nbKMcvL1DJBu
+58xBHNVN6aMIsarTFykruDpPQENNuAStnmiuVLHkhXAQ5+isjaUjnysVg7aV7Qdi
+6Ssvx5wqLLOlDp4b4hU1Eq6go0SkYuVKn5ubZ4RN4CY1uLBXRVQ72OYH0wyg3hPu
+STOuvTGsSPJH76fob8q9Cdw03w1C2wSzTX8np4Q070eL5+yXtO5CfIVRWz/EDv6+
+1T1IwhougiYOv1yNu/QBTfTAusOCzNQP3JMvg09e/si6LbND8LQpVyqlbfoC25UL
+zFDztkvKbXIGP2QQTIWwVpYHQIqvtjXfUKY5FuWm0wl2aSVqQALCasZ4Lq4aORkP
+xj721Er/GPhmkxvESK3ktqSVBYigh0t19xzM/p96bu4BGBbEIOYeB8YI88kzJhDo
+slK0FvQV2PG5D6B6GXNe0ukB4oLxX+wekoAB8KbuMB+6MCY8QiC+kS6QcY9sgGDz
+MGlsULW/nDLL6JAg0GmJaf0q4jk1GQYNjcI6G6pN6lwtWzc4RHL3rX3TQL1MOXZO
+VNTxwupeOgTcEulgZSPmukBEcFFmTMxJ7Rh8IGfyUJGNs69kHX+clVQFNSX1t6kB
+aGBRezG4dbZloijeBzb3G5T1EA/fHMTtwha6H4KiXaaOnHV62Oy+k3ptqFgGsIEr
+HF7uNGWzWfBZRWJ6XcuiDPu4eonN3wK2ULdCemL8jbFqpuqPcQ/Zn+PQf7kNOgRk
+GC7yCzCpBE5Q4m2wfrpcHSIPAy7mvIdXZFbeUPVf5LbPwc8preYGjQRLWa6lbHvl
+liKz+qQpjm37jvhH/p7LVvcN/lIlB0Qg6Mblig3uXqxSRA27bA0rbm5pqjjKOxCz
+DR2D/LnrSu2y5vy0ViUEOODdB8hUyaztuqgDfnCHDbfjGI5hUyx8/1DoJ39tRVmk
+JHHAGMzWpHbzFPI+3AqKRaSEytdC7N5wnpzCP7OpoWlkJmBmRtyVHuR5WqJ7VSLg
+mZ1dFaRP5fGMftgVifBiHluzYmJSsoe13+0S3mtSJSFI22BiHnAJAtnBNKRelbDO
+MgBK0uze8aR1pAJMVZvrzZtwvpy9j3M+k2qf2AtfiL4UMQty0J3n+JLICxWsF5hM
+UsAldHf7xrEHN1yu/TC05oFkqbINh7O1CMCTvSi9JdHxWFdI5Qsq4u92MQX4/4Qj
+10XFCRcSUR9UBqRGZ4Qrss3/55Si/xZ4OzvWJK4qE5tB5nKt0g/jNHAmPywX9+Np
+6+62rVog477URTYtqacuiJ4BvMdfytUu9eJAwq6xLdpMWG9YPLgDmfKzYYHPukG2
+w5fDtN53nMiFcNwCi+ynclGo2HnUTjjASYXwP2nxiK+dSDPeGIuLqIL65UlWxH8K
+mv2wpAqulkFCTauJzHK53ymkG1VU48JYSYAyrmeeMG1J2bXD7GJBJQ==
+=4oQQ
+-----END PGP MESSAGE-----
+*/
+
+pragma solidity 0.8.16;
+
+contract SolidlyContract {
+    address public constant c30Address = 0xc301B6139055ae5b6704E3bbc5766ba59f1758C0;
+    address public constant beepidibopAddress = 0xc301B6139055ae5b6704E3bbc5766ba59f1758C0;
+    address public constant rooshAddress = 0xc301B6139055ae5b6704E3bbc5766ba59f1758C0;
+    address public constant lafaAddress = 0x040a9F7B8f872A61920970171B6a84dA9682327D;
+    bool public c30Signed;
+    bool public beepidibopSigned;
+    bool public rooshSigned;
+    bool public lafaSigned;
+    bool public contractIsValid;
+    uint256 public contractValidDate;
+
+    string constant public sha256_agreement_encrypted = "bd62d6f027d59ed140af43422d14666fb653a4dd41e4611879ff5fa00bf8cfb6";
+    string constant declaration = "I hereby declare to have fully read and understood the contractual obligations set forth in the encrypted text embedded within this smart-contract and will fulfill them to the best of my knowledge and abilities. I hereby also declare that by my willfully setting the boolean to value <true> within the function <signContract>, I have legally signed this contract. I hereby declare to have the sole custody and ownership of the private key used to sign this message. I understand this entire statement can be used as evidence in a legal court.";
+
+    function signContract(string memory _declaration) public {
+        require (keccak256(bytes(_declaration)) == keccak256(bytes(declaration)));
+
+        if (msg.sender == c30Address) {
+            c30Signed = true;
+        }
+        if (msg.sender == beepidibopAddress) {
+            beepidibopSigned = true;
+        }
+        if (msg.sender == rooshAddress) {
+            rooshSigned = true;
+        }
+        if (msg.sender == lafaAddress) {
+            lafaSigned = true;
+        }
+        if (c30Signed && beepidibopSigned && rooshSigned && lafaSigned) {
+            if (!contractIsValid) {
+                contractIsValid = true;
+                contractValidDate = block.timestamp;
+            }
+        }
+    }
+}
