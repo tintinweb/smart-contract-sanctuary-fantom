3bf81b108276e50092b7733b37d1a2e979af12c2
minion <minion>
2022-08-20 05:13:24 +0000

    with 💕

---
+++ contracts/mainnet/2b/2b1c02b471d26f691d0edba0d65ef302ffd5ac4a_Sub2JunionOnYouTube.sol
@@ -0,0 +1,20 @@
+/**
+ *Submitted for verification at FtmScan.com on 2022-08-20
+*/
+
+/*
+░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░████╗░░░███╗░░██╗░░░
+░░░░░░╚═██║░░░████╗░██║░░░
+░░░░░░░░██║░░░██╔██╗██║░░░
+░░░██╗░░██║░░░██║╚████║░░░
+░░░╚█████║█████╔╝░╚███║░░░
+░░░░╚════╝╚════╝░░░╚══╝░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+*/
+pragma solidity 0.8.16;
+contract Sub2JunionOnYouTube {
+    function Sub2JunionOnYouTube_wuatcyecupza() external{
+        revert("https://youtube.com/Jun1on");
+    }
+}
