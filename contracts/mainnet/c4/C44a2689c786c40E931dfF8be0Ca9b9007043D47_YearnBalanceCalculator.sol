ce35a1a484bce13301822bd92b86260ba54ab311
minion <minion>
2022-10-11 17:23:24 +0000

    with 💕

---
+++ contracts/mainnet/c4/C44a2689c786c40E931dfF8be0Ca9b9007043D47_YearnBalanceCalculator.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "./interfaces/IBalanceCalculator.sol";
+
+interface Vault {
+	function pricePerShare() external view returns(uint256);
+}
+
+contract YearnBalanceCalculator is IBalanceCalculator {
+	function calculateTokenBalance(address baseToken, uint256 lpValue) external view returns(uint) {
+		Vault vault = Vault(baseToken);
+
+    	return vault.pricePerShare() * lpValue;
+	}
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+interface IBalanceCalculator {
+	function calculateTokenBalance(address baseToken, uint256 lpValue) external view returns(uint);
+}
