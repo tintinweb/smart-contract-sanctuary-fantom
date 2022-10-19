7aaabc5ccf1e103c5e906126203b6b91a30275b9
minion <minion>
2022-10-19 11:19:44 +0000

    with 💕

---
+++ contracts/mainnet/12/126E75609cb5B20D07e9250E38e5fC14dFb7851b_GelatoLickerDst.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+interface ILayerZeroReceiver {
+    // @notice LayerZero endpoint will invoke this function to deliver the message on the destination
+    // @param _srcChainId - the source endpoint identifier
+    // @param _srcAddress - the source sending contract address from the source chain
+    // @param _nonce - the ordered message nonce
+    // @param _payload - the signed payload is the UA bytes has encoded to be sent
+    function lzReceive(
+        uint16 _srcChainId,
+        bytes calldata _srcAddress,
+        uint64 _nonce,
+        bytes calldata _payload
+    ) external;
+}
+
+interface IIceCreamNFT {
+    function lick(uint256 _tokenId) external;
+}
+
+contract GelatoLickerDst is ILayerZeroReceiver {
+    address public constant lzEndpoint =
+        address(0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7); // fantom lz endpoint
+    IIceCreamNFT public constant iceCreamNFT =
+        IIceCreamNFT(0x255F82563b5973264e89526345EcEa766DB3baB2); // fantom IceCreamNFT address
+    address public constant srcLicker =
+        address(0xC8aE2A55bC4Bf3280a38C76077A43D4D4a086272); // avalanche gelato licker address
+
+    event CCLicked(
+        uint16 srcChainId,
+        address srcAddress,
+        uint256 indexed tokenId,
+        uint256 timestamp
+    );
+
+    function lzReceive(
+        uint16 _srcChainId,
+        bytes memory _srcAddress,
+        uint64,
+        bytes memory lickPayload
+    ) external override {
+        require(
+            msg.sender == address(lzEndpoint),
+            "CrossChainGelatoLicker: Only endpoint"
+        );
+        address srcAddress;
+        assembly {
+            srcAddress := mload(add(_srcAddress, 20))
+        }
+        require(
+            srcAddress == srcLicker,
+            "CrossChainGelatoLicker: Only srcLicker"
+        );
+
+        uint256 tokenId = abi.decode(lickPayload, (uint256));
+
+        iceCreamNFT.lick(tokenId);
+
+        emit CCLicked(_srcChainId, srcAddress, tokenId, block.timestamp);
+    }
+}
