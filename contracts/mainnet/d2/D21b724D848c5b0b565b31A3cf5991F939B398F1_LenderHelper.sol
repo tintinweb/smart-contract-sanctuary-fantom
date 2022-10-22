d5ee146e429626aeaf7e0560254588eaf91dd877
minion <minion>
2022-10-22 17:27:30 +0000

    with 💕

---
+++ contracts/mainnet/d2/D21b724D848c5b0b565b31A3cf5991F939B398F1_LenderHelper.sol
@@ -0,0 +1,46 @@
+/**
+ *Submitted for verification at FtmScan.com on 2022-10-22
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.17;
+
+
+interface IOracle {
+    function price() external view returns (uint256);
+}
+
+interface IDeiLenderSolidex {
+    function getLiquidationPrice(address user) external view returns (uint256);
+    function oracle() external view returns(IOracle);
+}
+
+contract LenderHelper {
+    IDeiLenderSolidex public ddLenderContract;
+    IDeiLenderSolidex public udLenderContract;
+
+    constructor(){
+        ddLenderContract = IDeiLenderSolidex(0x118FF56bb12E5E0EfC14454B8D7Fa6009487D64E);
+        udLenderContract = IDeiLenderSolidex(0x8D643d954798392403eeA19dB8108f595bB8B730);
+    }
+
+    function getDDOraclePrice() external view returns (uint256) {
+        return ddLenderContract.oracle().price();
+    }
+
+    function getUDOraclePrice() external view returns (uint256) {
+        return udLenderContract.oracle().price();
+    }
+
+    function getLiquidationPrices(address user) external view returns (uint256, uint256) {
+        return (ddLenderContract.getLiquidationPrice(user), udLenderContract.getLiquidationPrice(user));
+    }
+
+    function canLiquid(address user) external view returns (bool, bool) {
+        bool ddIsSolvent = ddLenderContract.getLiquidationPrice(user) >= ddLenderContract.oracle().price();
+        bool udIsSolvent = udLenderContract.getLiquidationPrice(user) >= udLenderContract.oracle().price();
+        return (ddIsSolvent, udIsSolvent);
+
+    }
+}
