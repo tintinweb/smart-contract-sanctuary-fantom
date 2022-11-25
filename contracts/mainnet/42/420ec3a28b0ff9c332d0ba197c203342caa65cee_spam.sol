f260eaa36869c64093fc0c9d3193fa08ed7ab97e
minion <minion>
2022-11-25 11:07:18 +0000

    with 💕

---
+++ contracts/mainnet/42/420ec3a28b0ff9c332d0ba197c203342caa65cee_spam.sol
@@ -0,0 +1,12 @@
+/**
+ *Submitted for verification at FtmScan.com on 2022-11-25
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract spam {
+    function sorry() external returns (bool) {
+        return true;
+    }
+}
