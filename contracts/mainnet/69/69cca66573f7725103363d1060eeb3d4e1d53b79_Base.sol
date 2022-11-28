6bf394b4cd898536adf6a2bc51c3d80dc8c284f6
minion <minion>
2022-11-28 05:07:47 +0000

    with 💕

---
+++ contracts/mainnet/69/69cca66573f7725103363d1060eeb3d4e1d53b79_Base.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Base {
+    uint256 base1;
+    uint256[49] __gap;
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import {Base} from './Base.sol';
+
+contract Child is Base {
+    uint256 child;
+}
