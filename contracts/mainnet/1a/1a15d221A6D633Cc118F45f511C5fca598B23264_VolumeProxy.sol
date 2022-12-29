1c8248e630c621d2d0b5989433fd28f253371f40
minion <minion>
2022-12-29 11:06:04 +0000

    with 💕

---
+++ contracts/mainnet/1a/1a15d221A6D633Cc118F45f511C5fca598B23264_VolumeProxy.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+contract VolumeProxy {
+
+    address public immutable router;
+    address public immutable WETH;
+
+    constructor(address _router,address _WETH){
+        router = _router;
+        WETH = _WETH;
+    }
+
+    receive() external payable {
+        assert(msg.sender == WETH); // only accept ETH via fallback from the WETH contract
+    }
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts) {
+
+        //bytes4 FUNC_SELECTOR = bytes4(keccak256("swapExactTokensForTokens(uint,uint,address[],address,uint)"));
+
+        bytes4 FUNC_SELECTOR = bytes4(keccak256(bytes('testSome(uint256)')));
+       // (bool success, bytes memory data) = router.call(abi.encodeWithSelector(FUNC_SELECTOR, amountIn, amountOutMin,path,to,deadline));
+        (bool success, bytes memory data) = router.call(abi.encodeWithSelector(FUNC_SELECTOR, 777));
+
+        // require(success && data.length > 0,'swap: SWAP_FAILED' );
+        return abi.decode(data, (uint []));
+    }
+}
