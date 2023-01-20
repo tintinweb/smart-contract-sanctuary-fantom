6d56dac9668f4c57ef9512899852302a919f5a8c
minion <minion>
2023-01-20 17:06:25 +0000

    with 💕

---
+++ contracts/mainnet/3c/3c0171594363505693c181b5ff56371fa7ce77ee_PseudoBuyback.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at FtmScan.com on 2023-01-20
+*/
+
+// SPDX-License-Identifier: GLWTPL
+pragma solidity ^0.8.17;
+
+contract PseudoBuyback {
+
+    function receiveAsset(address _tokenAddress, uint256 _amount) external {
+        // do nothing        
+    }
+}
