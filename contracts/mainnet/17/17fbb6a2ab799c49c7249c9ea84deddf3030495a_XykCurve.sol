06931fd85ef57267c9db3508c85871e9968fa968
minion <minion>
2023-01-23 05:07:39 +0000

    with 💕

---
+++ contracts/mainnet/17/17fbb6a2ab799c49c7249c9ea84deddf3030495a_XykCurve.sol
@@ -0,0 +1,430 @@
+/**
+ *Submitted for verification at FtmScan.com on 2023-01-22
+*/
+
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+
+
+
+library FixedPointMathLib {
+    /*///////////////////////////////////////////////////////////////
+                            COMMON BASE UNITS
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 internal constant YAD = 1e8;
+    uint256 internal constant WAD = 1e18;
+    uint256 internal constant RAY = 1e27;
+    uint256 internal constant RAD = 1e45;
+
+    /*///////////////////////////////////////////////////////////////
+                         FIXED POINT OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function fmul(
+        uint256 x,
+        uint256 y,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
+        assembly {
+            // Store x * y in z for now.
+            z := mul(x, y)
+
+            // Equivalent to require(x == 0 || (x * y) / x == y)
+            if iszero(or(iszero(x), eq(div(z, x), y))) {
+                revert(0, 0)
+            }
+
+            // If baseUnit is zero this will return zero instead of reverting.
+            z := div(z, baseUnit)
+        }
+    }
+
+    function fdiv(
+        uint256 x,
+        uint256 y,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
+        assembly {
+            // Store x * baseUnit in z for now.
+            z := mul(x, baseUnit)
+
+            if or(
+                // Revert if y is zero to ensure we don't divide by zero below.
+                iszero(y),
+                // Equivalent to require(x == 0 || (x * baseUnit) / x == baseUnit)
+                iszero(or(iszero(x), eq(div(z, x), baseUnit)))
+            ) {
+                revert(0, 0)
+            }
+
+            // We ensure y is not zero above, so there is never division by zero here.
+            z := div(z, y)
+        }
+    }
+
+    function fpow(
+        uint256 x,
+        uint256 n,
+        uint256 baseUnit
+    ) internal pure returns (uint256 z) {
+        assembly {
+            switch x
+            case 0 {
+                switch n
+                case 0 {
+                    z := baseUnit
+                }
+                default {
+                    z := 0
+                }
+            }
+            default {
+                switch mod(n, 2)
+                case 0 {
+                    z := baseUnit
+                }
+                default {
+                    z := x
+                }
+                let half := div(baseUnit, 2)
+                for {
+                    n := div(n, 2)
+                } n {
+                    n := div(n, 2)
+                } {
+                    let xx := mul(x, x)
+                    if iszero(eq(div(xx, x), x)) {
+                        revert(0, 0)
+                    }
+                    let xxRound := add(xx, half)
+                    if lt(xxRound, xx) {
+                        revert(0, 0)
+                    }
+                    x := div(xxRound, baseUnit)
+                    if mod(n, 2) {
+                        let zx := mul(z, x)
+                        if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) {
+                            revert(0, 0)
+                        }
+                        let zxRound := add(zx, half)
+                        if lt(zxRound, zx) {
+                            revert(0, 0)
+                        }
+                        z := div(zxRound, baseUnit)
+                    }
+                }
+            }
+        }
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        GENERAL NUMBER UTILITIES
+    //////////////////////////////////////////////////////////////*/
+
+    function sqrt(uint256 x) internal pure returns (uint256 result) {
+        if (x == 0) return 0;
+
+        result = 1;
+
+        uint256 xAux = x;
+
+        if (xAux >= 0x100000000000000000000000000000000) {
+            xAux >>= 128;
+            result <<= 64;
+        }
+
+        if (xAux >= 0x10000000000000000) {
+            xAux >>= 64;
+            result <<= 32;
+        }
+
+        if (xAux >= 0x100000000) {
+            xAux >>= 32;
+            result <<= 16;
+        }
+
+        if (xAux >= 0x10000) {
+            xAux >>= 16;
+            result <<= 8;
+        }
+
+        if (xAux >= 0x100) {
+            xAux >>= 8;
+            result <<= 4;
+        }
+
+        if (xAux >= 0x10) {
+            xAux >>= 4;
+            result <<= 2;
+        }
+
+        if (xAux >= 0x8) result <<= 1;
+
+        unchecked {
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+
+            uint256 roundedDownResult = x / result;
+
+            if (result > roundedDownResult) result = roundedDownResult;
+        }
+    }
+
+    function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        return x < y ? x : y;
+    }
+
+    function max(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        return x > y ? x : y;
+    }
+}
+
+interface ICurve {
+    /**
+        @notice Validates if a delta value is valid for the curve. The criteria for
+        validity can be different for each type of curve, for instance ExponentialCurve
+        requires delta to be greater than 1.
+        @param delta The delta value to be validated
+        @return valid True if delta is valid, false otherwise
+     */
+    function validateDelta(uint128 delta) external pure returns (bool valid);
+
+    /**
+        @notice Validates if a new spot price is valid for the curve. Spot price is generally assumed to be the immediate sell price of 1 NFT to the pool, in units of the pool's paired token.
+        @param newSpotPrice The new spot price to be set
+        @return valid True if the new spot price is valid, false otherwise
+     */
+    function validateSpotPrice(uint128 newSpotPrice)
+        external
+        view
+        returns (bool valid);
+
+    /**
+        @notice Given the current state of the pair and the trade, computes how much the user
+        should pay to purchase an NFT from the pair, the new spot price, and other values.
+        @param spotPrice The current selling spot price of the pair, in tokens
+        @param delta The delta parameter of the pair, what it means depends on the curve
+        @param numItems The number of NFTs the user is buying from the pair
+        @param feeMultiplier Determines how much fee the LP takes from this trade, 18 decimals
+        @param protocolFeeMultiplier Determines how much fee the protocol takes from this trade, 18 decimals
+        @return error Any math calculation errors, only Error.OK means the returned values are valid
+        @return newSpotPrice The updated selling spot price, in tokens
+        @return newDelta The updated delta, used to parameterize the bonding curve
+        @return inputValue The amount that the user should pay, in tokens
+        @return protocolFee The amount of fee to send to the protocol, in tokens
+     */
+    function getBuyInfo(
+        uint128 spotPrice,
+        uint128 delta,
+        uint256 numItems,
+        uint256 feeMultiplier,
+        uint256 protocolFeeMultiplier
+    )
+        external
+        view
+        returns (
+            CurveErrorCodes.Error error,
+            uint128 newSpotPrice,
+            uint128 newDelta,
+            uint256 inputValue,
+            uint256 protocolFee
+        );
+
+    /**
+        @notice Given the current state of the pair and the trade, computes how much the user
+        should receive when selling NFTs to the pair, the new spot price, and other values.
+        @param spotPrice The current selling spot price of the pair, in tokens
+        @param delta The delta parameter of the pair, what it means depends on the curve
+        @param numItems The number of NFTs the user is selling to the pair
+        @param feeMultiplier Determines how much fee the LP takes from this trade, 18 decimals
+        @param protocolFeeMultiplier Determines how much fee the protocol takes from this trade, 18 decimals
+        @return error Any math calculation errors, only Error.OK means the returned values are valid
+        @return newSpotPrice The updated selling spot price, in tokens
+        @return newDelta The updated delta, used to parameterize the bonding curve
+        @return outputValue The amount that the user should receive, in tokens
+        @return protocolFee The amount of fee to send to the protocol, in tokens
+     */
+    function getSellInfo(
+        uint128 spotPrice,
+        uint128 delta,
+        uint256 numItems,
+        uint256 feeMultiplier,
+        uint256 protocolFeeMultiplier
+    )
+        external
+        view
+        returns (
+            CurveErrorCodes.Error error,
+            uint128 newSpotPrice,
+            uint128 newDelta,
+            uint256 outputValue,
+            uint256 protocolFee
+        );
+}
+
+
+
+contract CurveErrorCodes {
+    enum Error {
+        OK, // No error
+        INVALID_NUMITEMS, // The numItem value is 0
+        SPOT_PRICE_OVERFLOW // The updated spot price doesn't fit into 128 bits
+    }
+}
+
+
+
+
+
+
+/*
+    @author 0xacedia
+    @notice Bonding curve logic for an x*y=k curve using virtual reserves.
+    @dev    The virtual token reserve is stored in `spotPrice` and the virtual nft reserve is stored in `delta`.
+            An LP can modify the virtual reserves by changing the `spotPrice` (tokens) or `delta` (nfts).
+*/
+contract XykCurve is ICurve, CurveErrorCodes {
+    using FixedPointMathLib for uint256;
+
+    /**
+        @dev See {ICurve-validateDelta}
+     */
+    function validateDelta(uint128 delta)
+        external
+        pure
+        override
+        returns (bool)
+    {
+        // all values are valid
+        return true;
+    }
+
+    /**
+        @dev See {ICurve-validateSpotPrice}
+     */
+    function validateSpotPrice(uint128 newSpotPrice)
+        external
+        pure
+        override
+        returns (bool)
+    {
+        // all values are valid
+        return true;
+    }
+
+    /**
+        @dev See {ICurve-getBuyInfo}
+     */
+    function getBuyInfo(
+        uint128 spotPrice,
+        uint128 delta,
+        uint256 numItems,
+        uint256 feeMultiplier,
+        uint256 protocolFeeMultiplier
+    )
+        external
+        pure
+        override
+        returns (
+            Error error,
+            uint128 newSpotPrice,
+            uint128 newDelta,
+            uint256 inputValue,
+            uint256 protocolFee
+        )
+    {
+        if (numItems == 0) {
+            return (Error.INVALID_NUMITEMS, 0, 0, 0, 0);
+        }
+
+        // get the pair's virtual nft and eth/erc20 reserves
+        uint256 tokenBalance = spotPrice;
+        uint256 nftBalance = delta;
+
+        // If numItems is too large, we will get divide by zero error
+        if (numItems >= nftBalance) {
+            return (Error.INVALID_NUMITEMS, 0, 0, 0, 0);
+        }
+
+        // calculate the amount to send in
+        uint256 inputValueWithoutFee = (numItems * tokenBalance) /
+            (nftBalance - numItems);
+
+        // add the fees to the amount to send in
+        protocolFee = inputValueWithoutFee.fmul(
+            protocolFeeMultiplier,
+            FixedPointMathLib.WAD
+        );
+        uint256 fee = inputValueWithoutFee.fmul(
+            feeMultiplier,
+            FixedPointMathLib.WAD
+        );
+        inputValue = inputValueWithoutFee + fee + protocolFee;
+
+        // set the new virtual reserves
+        newSpotPrice = uint128(spotPrice + inputValueWithoutFee); // token reserve
+        newDelta = uint128(nftBalance - numItems); // nft reserve
+
+        // If we got all the way here, no math error happened
+        error = Error.OK;
+    }
+
+    /**
+        @dev See {ICurve-getSellInfo}
+     */
+    function getSellInfo(
+        uint128 spotPrice,
+        uint128 delta,
+        uint256 numItems,
+        uint256 feeMultiplier,
+        uint256 protocolFeeMultiplier
+    )
+        external
+        pure
+        override
+        returns (
+            Error error,
+            uint128 newSpotPrice,
+            uint128 newDelta,
+            uint256 outputValue,
+            uint256 protocolFee
+        )
+    {
+        if (numItems == 0) {
+            return (Error.INVALID_NUMITEMS, 0, 0, 0, 0);
+        }
+
+        // get the pair's virtual nft and eth/erc20 balance
+        uint256 tokenBalance = spotPrice;
+        uint256 nftBalance = delta;
+
+        // calculate the amount to send out
+        uint256 outputValueWithoutFee = (numItems * tokenBalance) /
+            (nftBalance + numItems);
+
+        // subtract fees from amount to send out
+        protocolFee = outputValueWithoutFee.fmul(
+            protocolFeeMultiplier,
+            FixedPointMathLib.WAD
+        );
+        uint256 fee = outputValueWithoutFee.fmul(
+            feeMultiplier,
+            FixedPointMathLib.WAD
+        );
+        outputValue = outputValueWithoutFee - fee - protocolFee;
+
+        // set the new virtual reserves
+        newSpotPrice = uint128(spotPrice - outputValueWithoutFee); // token reserve
+        newDelta = uint128(nftBalance + numItems); // nft reserve
+
+        // If we got all the way here, no math error happened
+        error = Error.OK;
+    }
+}
---
+++ contracts/mainnet/ec/ec3F962238cC6D45aEc0f97D0f150e221Ef3C42C_SoulAura.sol
@@ -0,0 +1,372 @@
+/**
+ *Submitted for verification at FtmScan.com on 2023-01-22
+*/
+
+// SPDX-License-Identifier: MIT
+
+// File: contracts/interfaces/IPair.sol
+pragma solidity >=0.8.0;
+
+interface ISoulSwapPair {
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address owner) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint256);
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint256 amount0In,
+        uint256 amount1In,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint256);
+
+    function factory() external view returns (address);
+
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    
+    function getReserves() external view returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+
+    function price0CumulativeLast() external view returns (uint256);
+    function price1CumulativeLast() external view returns (uint256);
+    function kLast() external view returns (uint256);
+
+    function mint(address to) external returns (uint256 liquidity);
+    function burn(address to) external returns (uint256 amount0, uint256 amount1);
+
+    function swap(
+        uint256 amount0Out, 
+        uint256 amount1Out, 
+        address to, 
+        bytes calldata data
+    ) external;
+
+    function skim(address to) external;
+    function sync() external;
+
+    function initialize(address, address) external;
+}
+
+// File: contracts/interfaces/IERC20.sol
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    // EIP 2612
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
+
+// File: contracts/interfaces/ISummoner.sol
+
+interface ISummoner {
+    function userInfo(uint pid, address owner) external view returns (uint, uint);
+}
+
+// File: contracts/interfaces/IBond.sol
+
+interface IBond {
+    function pendingSoul(uint pid, address owner) external view returns (uint);
+}
+
+// File: @openzeppelin/contracts/utils/Context.sol
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// File: @openzeppelin/contracts/access/Ownable.sol
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// File: contracts/governance/SoulAura.sol
+pragma solidity >=0.8.0;
+
+contract SoulAura is Ownable {
+
+    ISummoner summoner = ISummoner(0xb898226dE3c5ca980381fE85F2Bc10e35e00634c);
+    IBond bond = IBond(0xEdaECfc744F3BDeAF6556AEEbfcDedd79437a01F);
+    IERC20 soul = IERC20(0xe2fb177009FF39F52C0134E8007FA0e4BaAcBd07);
+    IERC20 chant = IERC20(0xb844360d6CF54ed63FBA8C5aD06Cb00d4bdf46E0);
+
+    // SOULSWAP PAIRS
+    ISoulSwapPair soulNative = ISoulSwapPair(0xa2527Af9DABf3E3B4979d7E0493b5e2C6e63dC57);
+    ISoulSwapPair soulUsdc = ISoulSwapPair(0xC0A301f1E5E0Fe37a31657e8F60a41b14d01B0Ef);
+
+    // SOULSWAP FARMS
+    uint soulNativePid = 1;
+    uint soulUsdcPid = 2;
+
+    function name() public pure returns (string memory) { return "SoulAura"; }
+    function symbol() public pure returns (string memory) { return "AURA"; }
+    function decimals() public pure returns (uint8) { return 18; }
+
+    // gets: total voting power = twice the reserves in the LPs + soul supply + seance supply.
+    function totalSupply() public view returns (uint) {
+
+        // SOUL | SOUL-FTM + SOUL-USDC
+        (uint totalSoulFtm, , ) = soulNative.getReserves(); // token0 = SOUL
+        (uint totalSoulUsdc, , ) = soulUsdc.getReserves(); // token0 = SOUL
+
+        uint totalSoul = (2 * (totalSoulFtm + totalSoulUsdc)) + soul.totalSupply();
+
+        return totalSoul;
+    }
+
+    function balanceOf(address member) public view returns (uint) {
+        ( uint memberLiquidity, ) = pooledPower(member);
+        ( uint memberStake, ) = stakedPower(member);
+        ( uint memberSoul, ) = soulPower(member);
+        ( uint memberBond, ) = bondPower(member);
+
+        return memberLiquidity + memberStake + memberSoul + memberBond;
+    }
+
+    function soulBondPower(address member) public view returns (uint raw, uint formatted) {
+        uint ZERO = bond.pendingSoul(0, member); // FTM-SOUL
+        uint ONE = bond.pendingSoul(1, member); // USDC-SOUL
+        uint power = ZERO + ONE;
+
+        return (power, fromWei(power));
+    }
+
+    function swapBondPower(address member) public view returns (uint raw, uint formatted) {
+        // uint TWO = bond.pendingSoul(2, member); // SEANCE-FTM
+        uint THREE = bond.pendingSoul(3, member); // FTM-USDC
+        uint FOUR = bond.pendingSoul(4, member); // FTM-DAI
+        uint FIVE = bond.pendingSoul(5, member); // FTM-BNB
+        uint SIX = bond.pendingSoul(6, member); // FTM-ETH
+        uint SEVEN = bond.pendingSoul(7, member); // FTM-BTC
+        // uint EIGHT = bond.pendingSoul(8, member); // BTC-ETH
+        uint NINE = bond.pendingSoul(9, member); // USDC-DAI
+
+        uint power = THREE + FOUR + FIVE + SIX + SEVEN + NINE;
+
+        return (power, fromWei(power));
+    }
+
+    function lendBondPower(address member) public view returns (uint raw, uint formatted) {
+        uint TEN = bond.pendingSoul(10, member); // FTM-LEND
+        uint ELEVEN = bond.pendingSoul(11, member); // DAI-LEND
+        uint TWELVE = bond.pendingSoul(12, member); // ETH-LEND
+        uint THIRTEEN = bond.pendingSoul(13, member); // BNB-LEND
+
+        uint power = TEN + ELEVEN + TWELVE + THIRTEEN;
+        return (power, fromWei(power));
+    }
+
+    // gets: member's (total) bond power
+    function bondPower(address member) public view returns (uint raw, uint formatted) {
+        (uint SoulBondPower, ) = soulBondPower(member);
+        (uint SwapBondPower, ) = swapBondPower(member);
+        (uint LendBondPower, ) = lendBondPower(member);
+
+        uint power = SoulBondPower + SwapBondPower + LendBondPower;
+
+        return (power, fromWei(power));
+    }
+
+    // gets: member's pooled power
+    function pooledPower(address member) public view returns (uint raw, uint formatted) {
+        ( uint soulPooled, ) = pooledSoul(member);
+        uint power = soulPooled;
+
+        return (power, fromWei(power));
+
+    }
+
+    // gets: member's pooled SOUL power
+    function pooledSoul(address member) public view returns (uint raw, uint formatted) {
+        // total | LP tokens
+        uint lp_total =  
+              soulNative.totalSupply() 
+            + soulUsdc.totalSupply();
+
+        // total | pooled SOUL
+        uint lp_totalSoul =
+              soul.balanceOf(address(soulNative)) 
+            + soul.balanceOf(address(soulUsdc));
+
+        // member | SOUL LP balance
+        uint lp_walletBalance =
+              soulNative.balanceOf(member)
+            + soulUsdc.balanceOf(member);
+
+        // member | staked LP balance
+        (uint staked_soulNative, ) = summoner.userInfo(soulNativePid, member);
+        (uint staked_soulUsdc, ) = summoner.userInfo(soulUsdcPid, member);
+
+        uint lp_stakedBalance = staked_soulNative + staked_soulUsdc;
+
+        // member | lp balance
+        uint lp_balance = 
+              lp_walletBalance 
+            + lp_stakedBalance;
+
+        // LP voting power is 2X the members' SOUL share in the LP pool.
+        uint lp_power =
+              lp_totalSoul 
+            * lp_balance 
+            / lp_total * 2;
+
+        return (lp_power, fromWei(lp_power));
+
+    }
+
+    // gets: member's staked power
+    function stakedPower(address member) public view returns (uint raw, uint formatted) {
+        // stakedPower is the members' staked balance.
+        uint chantStaked = chant.balanceOf(member);
+        (uint manualStaked, ) = summoner.userInfo(0, member);
+        uint staked_power = chantStaked + manualStaked;
+        return (staked_power, fromWei(staked_power));
+    }
+
+    // gets: member's SOUL power
+    function soulPower(address member) public view returns (uint raw, uint formatted) {
+        // soul power is the members' SOUL balance.
+        uint soul_power = soul.balanceOf(member);
+        return (soul_power, fromWei(soul_power));
+    }
+
+    // blocks ERC20 functionality.
+    function allowance(address, address) public pure returns (uint) { return 0; }
+    function transfer(address, uint) public pure returns (bool) { return false; }
+    function approve(address, uint) public pure returns (bool) { return false; }
+    function transferFrom(address, address, uint) public pure returns (bool) { return false; }
+
+    // conversion helper functions
+    function toWei(uint intNum) public pure returns (uint bigInt) { return intNum * 10**18; }
+    function fromWei(uint bigInt) public pure returns (uint intNum) { return bigInt / 10**18; }
+}
