2f95109cc3ad2a0d0ec4ef79a4bdc470faa2b981
minion <minion>
2023-02-03 17:13:08 +0000

    with 💕

---
+++ contracts/mainnet/fe/fEbC2EBb5f60B7CDa7060dF243A2fBF63AB3b095_VaultMetaProvider.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "./interfaces/IVaultMetaProvider.sol";
+
+contract VaultMetaProvider {
+
+    string public _tokenURI;
+    string public baseURI;
+
+    constructor (string memory baseURI_, string memory tokenURI_) {
+        _tokenURI = tokenURI_;
+        baseURI = baseURI_;
+    }
+
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, _tokenURI)) : "";
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IVaultMetaProvider {
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+}
