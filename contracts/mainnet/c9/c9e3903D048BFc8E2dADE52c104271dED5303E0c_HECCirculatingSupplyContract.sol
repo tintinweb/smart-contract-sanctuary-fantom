36e1cd04be7529939dd19316c17c8967a8836628
minion <minion>
2023-05-24 05:04:31 +0000

    with 💕

---
+++ contracts/mainnet/c9/c9e3903D048BFc8E2dADE52c104271dED5303E0c_HECCirculatingSupplyContract.sol
@@ -0,0 +1,424 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+	/**
+	 * @dev Returns the addition of two unsigned integers, reverting on
+	 * overflow.
+	 *
+	 * Counterpart to Solidity's `+` operator.
+	 *
+	 * Requirements:
+	 *
+	 * - Addition cannot overflow.
+	 */
+	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a + b;
+		require(c >= a, 'SafeMath: addition overflow');
+
+		return c;
+	}
+
+	/**
+	 * @dev Returns the subtraction of two unsigned integers, reverting on
+	 * overflow (when the result is negative).
+	 *
+	 * Counterpart to Solidity's `-` operator.
+	 *
+	 * Requirements:
+	 *
+	 * - Subtraction cannot overflow.
+	 */
+	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		return sub(a, b, 'SafeMath: subtraction overflow');
+	}
+
+	/**
+	 * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+	 * overflow (when the result is negative).
+	 *
+	 * Counterpart to Solidity's `-` operator.
+	 *
+	 * Requirements:
+	 *
+	 * - Subtraction cannot overflow.
+	 */
+	function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+		require(b <= a, errorMessage);
+		uint256 c = a - b;
+
+		return c;
+	}
+
+	/**
+	 * @dev Returns the multiplication of two unsigned integers, reverting on
+	 * overflow.
+	 *
+	 * Counterpart to Solidity's `*` operator.
+	 *
+	 * Requirements:
+	 *
+	 * - Multiplication cannot overflow.
+	 */
+	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		// Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+		// benefit is lost if 'b' is also tested.
+		// See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+		if (a == 0) {
+			return 0;
+		}
+
+		uint256 c = a * b;
+		require(c / a == b, 'SafeMath: multiplication overflow');
+
+		return c;
+	}
+
+	/**
+	 * @dev Returns the integer division of two unsigned integers. Reverts on
+	 * division by zero. The result is rounded towards zero.
+	 *
+	 * Counterpart to Solidity's `/` operator. Note: this function uses a
+	 * `revert` opcode (which leaves remaining gas untouched) while Solidity
+	 * uses an invalid opcode to revert (consuming all remaining gas).
+	 *
+	 * Requirements:
+	 *
+	 * - The divisor cannot be zero.
+	 */
+	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+		return div(a, b, 'SafeMath: division by zero');
+	}
+
+	/**
+	 * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+	 * division by zero. The result is rounded towards zero.
+	 *
+	 * Counterpart to Solidity's `/` operator. Note: this function uses a
+	 * `revert` opcode (which leaves remaining gas untouched) while Solidity
+	 * uses an invalid opcode to revert (consuming all remaining gas).
+	 *
+	 * Requirements:
+	 *
+	 * - The divisor cannot be zero.
+	 */
+	function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+		require(b > 0, errorMessage);
+		uint256 c = a / b;
+		// assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+		return c;
+	}
+
+	/**
+	 * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+	 * Reverts when dividing by zero.
+	 *
+	 * Counterpart to Solidity's `%` operator. This function uses a `revert`
+	 * opcode (which leaves remaining gas untouched) while Solidity uses an
+	 * invalid opcode to revert (consuming all remaining gas).
+	 *
+	 * Requirements:
+	 *
+	 * - The divisor cannot be zero.
+	 */
+	function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+		return mod(a, b, 'SafeMath: modulo by zero');
+	}
+
+	/**
+	 * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+	 * Reverts with custom message when dividing by zero.
+	 *
+	 * Counterpart to Solidity's `%` operator. This function uses a `revert`
+	 * opcode (which leaves remaining gas untouched) while Solidity uses an
+	 * invalid opcode to revert (consuming all remaining gas).
+	 *
+	 * Requirements:
+	 *
+	 * - The divisor cannot be zero.
+	 */
+	function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+		require(b != 0, errorMessage);
+		return a % b;
+	}
+
+	// babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+	function sqrrt(uint256 a) internal pure returns (uint c) {
+		if (a > 3) {
+			c = a;
+			uint b = add(div(a, 2), 1);
+			while (b < c) {
+				c = b;
+				b = div(add(div(a, b), b), 2);
+			}
+		} else if (a != 0) {
+			c = 1;
+		}
+	}
+
+	/*
+	 * Expects percentage to be trailed by 00,
+	 */
+	function percentageAmount(uint256 total_, uint8 percentage_) internal pure returns (uint256 percentAmount_) {
+		return div(mul(total_, percentage_), 1000);
+	}
+
+	/*
+	 * Expects percentage to be trailed by 00,
+	 */
+	function substractPercentage(uint256 total_, uint8 percentageToSub_) internal pure returns (uint256 result_) {
+		return sub(total_, div(mul(total_, percentageToSub_), 1000));
+	}
+
+	function percentageOfTotal(uint256 part_, uint256 total_) internal pure returns (uint256 percent_) {
+		return div(mul(part_, 100), total_);
+	}
+
+	/**
+	 * Taken from Hypersonic https://github.com/M2629/HyperSonic/blob/main/Math.sol
+	 * @dev Returns the average of two numbers. The result is rounded towards
+	 * zero.
+	 */
+	function average(uint256 a, uint256 b) internal pure returns (uint256) {
+		// (a + b) / 2 can overflow, so we distribute
+		return (a / 2) + (b / 2) + (((a % 2) + (b % 2)) / 2);
+	}
+
+	function quadraticPricing(uint256 payment_, uint256 multiplier_) internal pure returns (uint256) {
+		return sqrrt(mul(multiplier_, payment_));
+	}
+
+	function bondingCurve(uint256 supply_, uint256 multiplier_) internal pure returns (uint256) {
+		return mul(multiplier_, supply_);
+	}
+}
+
+contract HECCirculatingSupplyContract is Ownable {
+	using SafeMath for uint;
+
+	bool public isInitialized;
+
+	address public HEC;
+	address[] public nonCirculatingHECAddresses;
+
+	constructor(address _hec) {
+		require(isInitialized == false);
+
+		HEC = _hec;
+
+		isInitialized = true;
+	}
+
+	function HECCirculatingSupply() external view returns (uint) {
+		uint _totalSupply = IERC20(HEC).totalSupply();
+
+		uint _circulatingSupply = _totalSupply.sub(getNonCirculatingHEC());
+
+		return _circulatingSupply;
+	}
+
+	function getNonCirculatingHEC() public view returns (uint) {
+		uint _nonCirculatingHEC;
+
+		for (uint i = 0; i < nonCirculatingHECAddresses.length; i = i.add(1)) {
+			_nonCirculatingHEC = _nonCirculatingHEC.add(IERC20(HEC).balanceOf(nonCirculatingHECAddresses[i]));
+		}
+
+		return _nonCirculatingHEC;
+	}
+
+	function setNonCirculatingHECAddresses(address[] calldata _nonCirculatingAddresses) external onlyOwner returns (bool) {
+		nonCirculatingHECAddresses = _nonCirculatingAddresses;
+
+		return true;
+	}
+}
