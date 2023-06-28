4d38d9916d1cd39ff578e48f68d733696cebf035
minion <minion>
2023-06-28 17:09:46 +0000

    with 💕

---
+++ contracts/mainnet/56/568367f421193352a2da075fbb485cDEFC22C435_Forwarder.sol
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ForwarderStorage.sol";
+import "./IForwarder.sol";
+
+contract Forwarder is IForwarder, Ownable, ForwarderStorage{
+
+  constructor() {}
+
+  function metaTransfer(bytes memory signature, bytes memory data, uint256 nonce, uint256 validTill) override public {
+      if (block.timestamp >= validTill) revert ExpiredSignatureError("Forwarder-metaTransfer: message signature expired!");
+      address signer = getSigner(metaTransferHash(data), signature);
+      if (signer == address(0)) revert InvalidAddressError("Signer address is invalid");
+      if (nonce != replayNonce[signer]) revert InvalidNonceError("Nonce does not match replayNonce for signer");
+      replayNonce[signer]++;
+      bytes memory modifiedData = bytes.concat(data, abi.encode(signer));
+      (bool success, bytes memory resData) = revenantStash.call(modifiedData);
+      if (!success) revert TransactionFailedError("Transaction NSF.");
+  }
+  function setRevenantStash(address _revenantStash) override external{
+    revenantStash=_revenantStash;
+  }
+
+  function metaTransferHash(bytes memory data) override public pure returns (bytes32) {
+    return keccak256(abi.encodePacked(data));
+  }
+
+  function getSigner(bytes32 _hash, bytes memory _signature)  internal pure returns (address){
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    if (_signature.length != 65) {
+        return address(0);
+    }
+    assembly {
+        r := mload(add(_signature, 32))
+        s := mload(add(_signature, 64))
+        v := byte(0, mload(add(_signature, 96)))
+    }
+    if (v < 27) {
+        v += 27;
+    }
+    if (v != 27 && v != 28) {
+        return address(0);
+    } else {
+      return ecrecover(
+        keccak256(
+          abi.encodePacked(
+              "\x19Ethereum Signed Message:\n32",
+              _hash
+          )
+        ),
+        v,
+        r,
+        s
+      );
+    }
+  }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IForwarder{
+    function metaTransfer(bytes memory signature, bytes memory data, uint256 nonce, uint256 validTill) external;
+    function metaTransferHash(bytes memory data) external pure returns (bytes32);
+    function setRevenantStash(address revenantStashAddress) external;
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract ForwarderStorage{
+  mapping (address => uint256) public replayNonce;
+  address public revenantStash;
+ 
+  error ExpiredSignatureError(string);
+  error InvalidAddressError(string);
+  error InvalidNonceError(string);
+  error TransactionFailedError(string);
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.9.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
---
+++ contracts/mainnet/f4/F4aa3c6AFd61a79fdc91C9654b81F0f8890Ac25e_RevenantStash.sol
@@ -0,0 +1,197 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./RevenantStashStorage.sol";
+import "./IRevenantStash.sol";
+import "./opengsn/BaseRelayRecipient.sol";
+import './RevenantStashStructs.sol';
+
+
+contract RevenantStash is RevenantStashStorage,IRevenantStash,BaseRelayRecipient {
+
+    string public constant override versionRecipient = "2.2.0";
+
+    constructor(address _relayer,address _forwarder) {
+        if (_relayer == address(0)) revert RelayerCannotBeZero();
+        relayer = _relayer;
+        owner = msg.sender;
+        _setTrustedForwarder(_forwarder);
+    }
+    function sendFile(address recipient, string memory cid,string memory name) override public {
+        address sender = _msgSender();
+        if (recipient == address(0)) revert RecipientCannotBeZero();
+        if (bytes(cid).length <= 0) revert CIDCannotBeEmpty();
+        stash[recipient].push(Stash(sender, cid, name));
+        emit FileReceived(recipient, cid);
+    }
+    function deleteFile(uint index) override public {
+        address sender = _msgSender();
+        require(index < stash[sender].length, "Index out of bounds");
+        stash[sender][index] = stash[sender][stash[sender].length - 1];
+        stash[sender].pop();
+        emit FileDeleted(index,sender);
+    }
+    function numberOfFiles(address recipient) override external view returns(uint256){
+        return stash[recipient].length;
+    }
+
+    function changeRelayer(address newRelayer) override  public onlyOwner {
+        if (newRelayer == address(0)) revert RelayerCannotBeZero();
+        relayer = newRelayer;
+        emit RelayerChanged(newRelayer);
+    }
+    function getFiles(address recipient) override public view returns (Stash[] memory) {
+        return stash[recipient];
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+/**
+ * a contract must implement this interface in order to support relayed transaction.
+ * It is better to inherit the BaseRelayRecipient as its implementation.
+ */
+abstract contract IRelayRecipient {
+
+    /**
+     * return if the forwarder is trusted to forward relayed transactions to us.
+     * the forwarder is required to verify the sender's signature, and verify
+     * the call is not a replay.
+     */
+    function isTrustedForwarder(address forwarder) public virtual view returns(bool);
+
+    /**
+     * return the sender of this call.
+     * if the call came through our trusted forwarder, then the real sender is appended as the last 20 bytes
+     * of the msg.data.
+     * otherwise, return `msg.sender`
+     * should be used in the contract anywhere instead of msg.sender
+     */
+    function _msgSender() internal virtual view returns (address);
+
+    /**
+     * return the msg.data of this call.
+     * if the call came through our trusted forwarder, then the real sender was appended as the last 20 bytes
+     * of the msg.data - so this method will strip those 20 bytes off.
+     * otherwise (if the call was made directly and not through the forwarder), return `msg.data`
+     * should be used in the contract instead of msg.data, where this difference matters.
+     */
+    function _msgData() internal virtual view returns (bytes calldata);
+
+    function versionRecipient() external virtual view returns (string memory);
+}
+
+// SPDX-License-Identifier: MIT
+// solhint-disable no-inline-assembly
+pragma solidity >=0.6.9;
+
+import "./IRelayRecipient.sol";
+
+/**
+ * A base contract to be inherited by any contract that want to receive relayed transactions
+ * A subclass must use "_msgSender()" instead of "msg.sender"
+ */
+abstract contract BaseRelayRecipient is IRelayRecipient {
+
+    /*
+     * Forwarder singleton we accept calls from
+     */
+    address private _trustedForwarder;
+
+    function trustedForwarder() public virtual view returns (address){
+        return _trustedForwarder;
+    }
+
+    function _setTrustedForwarder(address _forwarder) internal {
+        _trustedForwarder = _forwarder;
+    }
+
+    function isTrustedForwarder(address forwarder) public virtual override view returns(bool) {
+        return forwarder == _trustedForwarder;
+    }
+
+    /**
+     * return the sender of this call.
+     * if the call came through our trusted forwarder, return the original sender.
+     * otherwise, return `msg.sender`.
+     * should be used in the contract anywhere instead of msg.sender
+     */
+    function _msgSender() internal override virtual view returns (address ret) {
+        if (msg.data.length >= 20 && isTrustedForwarder(msg.sender)) {
+            // At this point we know that the sender is a trusted forwarder,
+            // so we trust that the last bytes of msg.data are the verified sender address.
+            // extract sender address from the end of msg.data
+            assembly {
+                ret := shr(96,calldataload(sub(calldatasize(),20)))
+            }
+        } else {
+            ret = msg.sender;
+        }
+    }
+
+    /**
+     * return the msg.data of this call.
+     * if the call came through our trusted forwarder, then the real sender was appended as the last 20 bytes
+     * of the msg.data - so this method will strip those 20 bytes off.
+     * otherwise (if the call was made directly and not through the forwarder), return `msg.data`
+     * should be used in the contract instead of msg.data, where this difference matters.
+     */
+    function _msgData() internal override virtual view returns (bytes calldata ret) {
+        if (msg.data.length >= 20 && isTrustedForwarder(msg.sender)) {
+            return msg.data[0:msg.data.length-20];
+        } else {
+            return msg.data;
+        }
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+struct Stash {
+    address relayedBy;
+    string cid;
+    string name;
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import './RevenantStashStructs.sol';
+
+contract RevenantStashStorage{
+    mapping (address => Stash[]) public stash;
+    address public relayer;
+    address public owner;
+
+    event FileReceived(address indexed recipient, string cid);
+    event RelayerChanged(address indexed newRelayer);
+    event FileDeleted(uint index,address indexed deleter);
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+    modifier onlyRelayer() {
+        require(msg.sender == relayer, "Caller is not the relayer");
+        _;
+    }
+
+    error RelayerCannotBeZero();
+    error RecipientCannotBeZero();
+    error CIDCannotBeEmpty();
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import './RevenantStashStructs.sol';
+
+
+interface IRevenantStash{
+    function sendFile(address recipient, string memory cid,string memory name) external;
+    function changeRelayer(address newRelayer) external;
+    function getFiles(address recipient) external view returns (Stash[] memory);
+    function deleteFile(uint index) external;
+    function numberOfFiles(address recipient)external returns(uint256);
+}
